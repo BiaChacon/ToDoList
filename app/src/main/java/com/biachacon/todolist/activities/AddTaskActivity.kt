2d4492bbe69d34d6b736f87fa725c45ce767f1e9
biachacon <anabheatrizchacon@outlook.com>
2019-11-14 18:54:21 -0300

    add task

--- app/src/main/java/com/biachacon/todolist/activities/AddTaskActivity.kt
+++ app/src/main/java/com/biachacon/todolist/activities/AddTaskActivity.kt
@@ -4,26 +4,40 @@ import android.app.Activity
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.room.Room
 import com.biachacon.todolist.R
+import com.biachacon.todolist.database.AppDatabase
 import com.biachacon.todolist.dialogs.AddListDialogFragment
+import com.biachacon.todolist.model.Task
+import com.biachacon.todolist.model.ToDoList
 import kotlinx.android.synthetic.main.activity_add_task.*
 import kotlinx.android.synthetic.main.activity_add_task.view.*
 import java.util.*
 
-class AddTaskActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener
-{
+class AddTaskActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
-    var list_of_items = arrayOf("Item 1", "Item 2", "Item 3")
+    val db: AppDatabase by lazy {
+        Room.databaseBuilder(this, AppDatabase::class.java, "to-do-list")
+            .allowMainThreadQueries()
+            .build()
+    }
+
+    lateinit var list:Array<String?>
     var l: String = ""
+    var date:String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
+
+        val actionBar = supportActionBar
+        actionBar!!.title = "New Task"
 
         //Calendar
         val c = Calendar.getInstance()
@@ -33,17 +47,23 @@ class AddTaskActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener
 
         datePicker.setOnClickListener{
             val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{view, mYear, mMonth, mDay ->
-               // dateChoice.setText(""+ mDay+"/"+mMonth+"/"+mYear)
-
+                dateChoice.setText(""+ mDay+"/"+mMonth+"/"+mYear)
+                date = ""+mDay+"/"+mMonth+"/"+mYear
             }, year, month, day)
             dpd.show()
         }
 
+
+        list = Array(db.toDoListDao().listAll().size) { null }
+        var j=0
+        for (i in db.toDoListDao().listAll()){
+            list.set(j, value = i.name)
+            j++
+        }
         spinner!!.setOnItemSelectedListener(this)
-        val array_adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, list_of_items)
+        val array_adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, list)
         array_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner!!.setAdapter(array_adapter)
-
 
         addNewlist.setOnClickListener {
             var dialog = AddListDialogFragment()
@@ -59,20 +79,34 @@ class AddTaskActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.getItemId()
-        if (id == R.id.action_one) {
-            setResult(Activity.RESULT_OK)
-            finish()
-            return true
+        return when(id){
+            R.id.cancel -> {
+                setResult(Activity.RESULT_CANCELED)
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
-        return super.onOptionsItemSelected(item)
     }
 
     override fun onItemSelected(arg0: AdapterView<*>, arg1: View, position: Int, id: Long) {
-        l = list_of_items[position]
+        l = list[position]!!
     }
 
     override fun onNothingSelected(arg0: AdapterView<*>) {
 
+    }
+
+    fun saveTask(view: View) {
+        var toDoList:ToDoList = db.toDoListDao().findByName(l)
+        db.taskDao().insert(Task(
+            addTask.text.toString(),
+            date,
+            false,
+            toDoList.id
+        ))
+        setResult(Activity.RESULT_OK)
+        finish()
     }
 
 }
